7d88e3083a6f167e8f1c5c758f1bd54c552f9bd6
Yamba Ouandaogo <yamba.ouandaogo@outlook.com>
2023-05-25 13:14:35 -0500

    refactoring exercise

--- src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
+++ src/main/java/com/codeup/codeupspringblog/controllers/PostController.java
@@ -1,22 +1,38 @@
 package com.codeup.codeupspringblog.controllers;
 
+import com.codeup.codeupspringblog.models.Post;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
 
 @Controller
 public class PostController {
 
     @GetMapping("/posts")
-    public String allPosts() {
-        return "posts";
+    public String allPosts(Model model) {
+        ArrayList<Post> posts = new ArrayList<>();
+        Post post1 = new Post("Title 1", "Body 1");
+        Post post2 = new Post("Title A", "Body A");
+        posts.add(post2);
+        posts.add(post1);
+
+        model.addAttribute("posts", posts);
+
+        return "posts/index";
     }
 
 
     @GetMapping("/posts/{id}")
-    public String individualPost() {
-        return "view an individual post";
+    public String individualPost(Model model) {
+        Post post = new Post("The only post", "The only body");
+
+        model.addAttribute("post", post);
+
+        return "posts/show";
     }
 
     @GetMapping("/posts/create")
@@ -31,3 +47,4 @@ public class PostController {
         return "create a new post";
     }
 }
+
